b67260916ac21fed76fc260d7a658a676d577c60
Joyel <Varghesejoyel71@gmail.com>
2026-06-20 00:47:59 +0530

    Feat: updated changes as per the new developer

--- site-manager-backend/src/routes/ping.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/ping", (req, res) => {
-  res.json({ status: "alive", time: new Date() });
-});
-
-module.exports = router;
